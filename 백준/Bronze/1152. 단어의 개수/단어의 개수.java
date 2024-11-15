ea6e659a8ec6e501ce99e88bf9d7bd5cc6c93e64
Yang Hanseo <hsyang011@gmail.com>
2024-11-15 09:42:16 +0900

    [Bronze II] Title: 단어의 개수, Time: 276 ms, Memory: 52816 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,14 +1,9 @@
 import java.io.*;
-
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String input = reader.readLine().trim();
-        
-        if (input.isEmpty()) {
-            System.out.println(0); // 빈 문자열이면 단어 개수는 0개
-        } else {
-            System.out.println(input.split(" ").length);
-        }
+        String str = reader.readLine().trim();
+        System.out.println(str.length() == 0 ? 0 : str.split(" ").length);
     }
 }
